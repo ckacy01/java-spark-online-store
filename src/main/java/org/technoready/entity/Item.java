0ae696b58e92364f720d7c9380d4d04d23000383
ckacy01 <ckacy01@protonmail.com>
2025-10-31 11:35:01 -0600

    feat: Creating the item entity

---
+++ src/main/java/org/technoready/entity/Item.java
@@ -0,0 +1,18 @@
+package org.technoready.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+public class Item {
+    private long id;
+    private String name;
+    private String description;
+    private double price;
+}
