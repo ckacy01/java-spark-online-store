3c81807281c3386eae7dd1e842dfc6cd41ebb48a
ckacy01 <ckacy01@protonmail.com>
2025-10-31 10:21:18 -0600

    feat: Impl the new methods

--- src/main/java/org/technoready/service/impl/UserServiceImpl.java
+++ src/main/java/org/technoready/service/impl/UserServiceImpl.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.jdbi.v3.core.Jdbi;
 import org.technoready.dao.UserDao;
 import org.technoready.dto.request.CreateUserRequest;
+import org.technoready.dto.request.UpdateUserRequest;
 import org.technoready.entity.User;
 import org.technoready.service.UserService;
 import org.technoready.util.UserMapper;
@@ -57,5 +58,45 @@ public class UserServiceImpl implements UserService {
     public Optional<User> getUserById(Long id) {
         log.debug("Fetching user with id: {}", id);
         return jdbi.withExtension(UserDao.class, dao -> dao.findById(id));
+    }
+
+    @Override
+    public User updateUser(Long id, UpdateUserRequest request) {
+        log.info("Updating user with id: {}", id);
+
+        // Validate request
+        request.validate();
+
+        // Find existing user
+        User user = getUserById(id)
+                .orElseThrow(() -> new IllegalArgumentException("User not found with id: " + id));
+
+        // Update user
+        User updatedUser = UserMapper.updateEntity(user, request);
+        jdbi.useExtension(UserDao.class, dao -> dao.update(updatedUser));
+
+        log.info("User updated successfully with id: {}", id);
+        return updatedUser;
+    }
+
+    @Override
+    public boolean deleteUser(Long id) {
+        log.info("Deleting user with id: {}", id);
+
+        int rowsAffected = jdbi.withExtension(UserDao.class, dao -> dao.delete(id));
+
+        if (rowsAffected > 0) {
+            log.info("User deleted successfully with id: {}", id);
+            return true;
+        }
+
+        log.warn("No user found to delete with id: {}", id);
+        return false;
+    }
+
+    @Override
+    public boolean userExists(Long id) {
+        log.debug("Checking if user exists with id: {}", id);
+        return jdbi.withExtension(UserDao.class, dao -> dao.exists(id));
     }
 }
